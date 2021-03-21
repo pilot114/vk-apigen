41ecc024ec07637302284505f898deac273ee6e1
Oleg Chulkov <pilot114@bk.ru>
2021-03-21 13:27:13 +0700

    hide

--- bin/example.php
+++ bin/example.php
@@ -10,13 +10,16 @@ function xr($string, $key) {
 
 // Authorization Code Flow ключ пользователя
 // https://oauth.vk.com/authorize?client_id=4664424&display=page&redirect_uri=http://85.143.221.59/callback&scope=4095&response_type=code&v=5.130
-// https://oauth.vk.com/access_token?client_id=4664424&client_secret=TBWQ9VvbXEOzJyivdViz&redirect_uri=http://85.143.221.59/callback&code=15dfb2834042aaefea
+// https://oauth.vk.com/access_token?client_id=4664424&client_secret=***&redirect_uri=http://85.143.221.59/callback&code=15dfb2834042aaefea
 
 // FILL DEFAULT
 $masterPass = '';
 $accessToken = xr(base64_decode('Qh1ICQBoPT8bT0wHATxgb0hNTwUKPGFtH0sYBgc7PWwZG04CUD9rbUpOSVIGOT5uSUpIBgBqbG9KSR4DUG45bUxLSwVSbDxjSBtPBwZoa20eSx9VBQ=='), $masterPass);
 
 $vk = new \VkApigen\Api($accessToken, 'user');
+
+$response = $vk->streaming()->
+
 
 $response = $vk->video()->search()->_adult(true)->_sort(1)->_count(200)
     ->q('Mr. Nobody')
